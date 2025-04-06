a677a6f8849a3e6065da164bfda24ae33980fee1
Huzaifa Qureshi <hq1432005@gmail.com>
2025-04-06 14:24:25 +0500

    Number class ready and optimized

--- number.py
+++ number.py
@@ -1,8 +1,11 @@
-from math import sqrt
+from math import isqrt
 
 class Number:
     def __init__(self, number: int):
+        """Constructor"""
         self.number = number
+        self.__absolute_number = abs(self.number)
+
         self.__sum_of_digits: int = 0
         self.__total_digits: int = 0
 
@@ -10,8 +13,12 @@ class Number:
     
     def __digits_logic(self):
         """Calculates total digits and their sum"""
-        temp_num = abs(self.number)
+        temp_num = self.__absolute_number
 
+        if temp_num == 0:
+            self.__total_digits = 1
+            return
+        
         while temp_num != 0:
             #extract last digit and add to total
             self.__sum_of_digits += temp_num % 10
@@ -21,20 +28,14 @@ class Number:
 
     def is_even(self)->bool:
         """Checks if number is even or not"""
-
-        if self.number%2 == 0:
-            return True
-        else:
-            return False
+        return self.number % 2 == 0
 
     def digit_count(self)->int:
         """Returns number of digits in number: ignoring negative sign"""
-
         return self.__total_digits
     
     def sum_of_digits(self)->int:
         """Returns sum of digits of number: ignoring negative sign"""
-
         return self.__sum_of_digits
     
     def is_prime(self)->bool:
@@ -49,7 +50,7 @@ class Number:
         elif self.number % 2 == 0:
             return False
         else:
-            for i in range(3, sqrt(self.number)+1, 2):
+            for i in range(3, isqrt(self.number)+1, 2):
                 if self.number % i == 0:
                     return False
             return True
@@ -57,7 +58,10 @@ class Number:
     def binary_representation(self)->str:
         """Returns binary version of number"""
 
-        bin_str = bin(abs(self.number))
+        if self.number == 0:
+            return '0'
+        
+        bin_str = bin(self.__absolute_number)
         #omit 0b prefix
         if self.number < 0:
             return '-' + bin_str[2:]
@@ -68,7 +72,7 @@ class Number:
         """Checks if number is palindrome or not: ignores negative sign"""
 
         #ignore negative sign
-        num = abs(self.number)
+        num = self.__absolute_number
 
         reversed_num = 0
         original_num = num
@@ -88,11 +92,11 @@ class Number:
         #ARMSTRONG NUMBER:
         # 153: (1**3) + (5**3) + (3**3) == 153 (True)
         
-        temp_num = abs(self.number)
+        temp_num = self.__absolute_number
         armstrong_sum = 0
 
         while temp_num != 0:
             armstrong_sum += (temp_num%10)**(self.__total_digits)
             temp_num //= 10
         
-        return armstrong_sum == self.number
+        return armstrong_sum == self.__absolute_number

7c55577fe592323226d03bd5c74611ef5c09df3d
Huzaifa Qureshi <hq1432005@gmail.com>
2025-04-06 14:40:46 +0500

    endpoint created

--- extra.py
+++
@@ -1,9 +0,0 @@
-from number import Number
-
-numbers = [153, 370, 371, 407, 9474]
-
-for i in numbers:
-    obj = Number(i)
-    if obj.is_armstrong() ==  False:
-        print('Test failed')
-        break
--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI, Path
 from typing import Annotated
 from number import Number
-
+from schema import NumberInsightResponse
 
 app = FastAPI()
 
-@app.get('/get-insights/{number}')
+@app.get('/get-insights/{number}', response_model=NumberInsightResponse)
 def insights(
     number: Annotated[
         int,
@@ -18,5 +18,15 @@ def insights(
         )
     ]
 ):
-    number = Number()
-    
+    num = Number(number)
+
+    return NumberInsightResponse(
+        number= num.number,
+        is_even= num.is_even(),
+        is_prime= num.is_prime(),
+        digit_count= num.digit_count(),
+        sum_of_digits= num.sum_of_digits(),
+        binary_representation= num.binary_representation(),
+        is_palindrome= num.is_palindrome(),
+        is_armstrong= num.is_armstrong()
+    )

49f60209abdfc5445c20896f531546defbe5b3b5
Huzaifa Qureshi <hq1432005@gmail.com>
2025-04-06 16:19:57 +0500

    Specific Insight Feature Added

---
+++ insight_enum.py
@@ -0,0 +1,10 @@
+from enum import Enum
+
+class SpecificInsight(Enum):
+    is_even = "Even Number Check"
+    is_prime = "Prime Number Check"
+    digit_count = "Count Digits"
+    sum_of_digits = "Sum of Digits"
+    bin_rep = "Binary Representation"
+    is_palindrome = "Palindrome Check"
+    is_armstrong = "ArmStrong Check"
--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-from fastapi import FastAPI, Path
+from fastapi import FastAPI, Path, Query
 from typing import Annotated
 from number import Number
 from schema import NumberInsightResponse
+from insight_enum import SpecificInsight
 
 app = FastAPI()
 
-@app.get('/get-insights/{number}', response_model=NumberInsightResponse)
+@app.get('/get-insights/{number}/', response_model=NumberInsightResponse | dict)
 def insights(
     number: Annotated[
         int,
@@ -16,10 +17,27 @@ def insights(
             description='The number to calculate insights for',
             example=45
         )
-    ]
+    ],
+    specific: SpecificInsight | None = None
 ):
     num = Number(number)
 
+    if specific: #query applied
+        if specific == SpecificInsight.is_even:
+            return {"is_even": num.is_even()}
+        elif specific == SpecificInsight.is_prime:
+            return {"is_prime": num.is_prime()}
+        elif specific == SpecificInsight.digit_count:
+            return {"digit_count": num.digit_count()}
+        elif specific == SpecificInsight.sum_of_digits:
+            return {"sum_of_digits": num.sum_of_digits()}
+        elif specific == SpecificInsight.bin_rep:
+            return {"binary_representation": num.binary_representation()}
+        elif specific == SpecificInsight.is_palindrome:
+            return {"is_palindrome": num.is_palindrome()}
+        elif specific == SpecificInsight.is_armstrong:
+            return {"is_armstrong": num.is_armstrong()}
+        
     return NumberInsightResponse(
         number= num.number,
         is_even= num.is_even(),
